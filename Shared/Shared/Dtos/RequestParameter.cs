91898dabbb6efb0c33b29a3ee04cc13904909e9a
Cemal Bulak <cemalbulak41@gmail.com>
2022-08-20 15:22:57 +0300

    Request Parameter set default value

--- Shared/Shared/Dtos/RequestParameter.cs
+++ Shared/Shared/Dtos/RequestParameter.cs
@@ -2,11 +2,12 @@
 
 public class RequestParameter
 {
-    public RequestParameter(int skip, int take)
+    public RequestParameter(int skip = 0, int take = 10)
     {
         Skip = skip;
         Take = take;
     }
+
     public int Skip { get; set; }
     public int Take { get; set; }
 }
